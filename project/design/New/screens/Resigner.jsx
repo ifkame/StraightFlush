2a6bc5fc89b4ff51f2195291300eaf3b8576b6e4
letanloc <letanlocjp1001@gmail.com>
2022-06-10 14:51:58 +0900

    update

---
+++ project/design/New/screens/Resigner.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Resigner = () => {
+  return (
+    <View>
+      <Text>Resigner</Text>
+    </View>
+  )
+}
+
+export default Resigner
+
+const styles = StyleSheet.create({})
