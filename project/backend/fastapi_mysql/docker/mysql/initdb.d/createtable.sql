2805e193de03b51c90b54d458d96d5a9b1dd15e8
2200241 <2200241@ecc.ac.jp>
2022-07-01 16:00:22 +0900

    sqlの変更

--- project/backend/fastapi_mysql/docker/mysql/initdb.d/createtable.sql
+++ project/backend/fastapi_mysql/docker/mysql/initdb.d/createtable.sql
@@ -1,5 +1,3 @@
-
-
 -- ユーザー
 CREATE TABLE users(
     user_id INT AUTO_INCREMENT,
@@ -8,11 +6,10 @@ CREATE TABLE users(
     password VARCHAR(64),
     point INT DEFAULT 0 NOT NULL,
     PRIMARY KEY(user_id),
-    UNIQUE(fcm_token),
-    UNIQUE(mail)
+    UNIQUE(fcm_token)
 );
 
--- 店舗
+-- 店舗 -- 緯度経度が複合ユニークになっていたので別々にしました
 CREATE TABLE stores(
      store_id INT AUTO_INCREMENT,
      name VARCHAR(30) NOT NULL,
@@ -25,8 +22,9 @@ CREATE TABLE stores(
      longitude DOUBLE(9,6) NOT NULL,
      opening_at DATE NOT NULL,
      PRIMARY KEY(store_id),
-     UNIQUE(qr_path),
-     UNIQUE(latitude,longitude)
+	UNIQUE(qr_path),
+     UNIQUE(latitude),
+	UNIQUE(longitude)
 );
 
 -- 商品
@@ -50,14 +48,30 @@ CREATE TABLE events(
      end_at DATETIME,
      func_name VARCHAR(30) NOT NULL,
      point INT NOT NULL,
-     PRIMARY KEY(event_id)
+     PRIMARY KEY(event_id),
+     UNIQUE(name)
+);
+
+-- スタンプ
+CREATE TABLE stamps(
+	user_id INT NOT NULL,
+	product_id INT NOT NULL,
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP 
+				ON UPDATE CURRENT_TIMESTAMP,
+	CONSTRAINT fk_stamps_users
+    	FOREIGN KEY (user_id)
+    	REFERENCES users(user_id),
+	CONSTRAINT fk_stamps_product
+    	FOREIGN KEY (product_id) 
+    	REFERENCES products(product_id)
 );
 
 -- イベントログ
 CREATE TABLE event_logs(
      user_id INT NOT NULL,
      event_id INT NOT NULL,
-     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP 
+                ON UPDATE CURRENT_TIMESTAMP,
      CONSTRAINT fk_eventLg_users
      FOREIGN KEY (user_id) 
      REFERENCES users(user_id),
@@ -66,24 +80,14 @@ CREATE TABLE event_logs(
      REFERENCES events(event_id)
 );
 
--- スタンプ
-CREATE TABLE stamps(
-	user_id INT NOT NULL,
-	img_path VARCHAR(50),
-	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	CONSTRAINT fk_stamps_users
-    	FOREIGN KEY (user_id)
-    	REFERENCES users(user_id),
-     UNIQUE(img_path)
-);
-
 -- 通知ログ
 CREATE TABLE notice_logs(
      notice_id INT AUTO_INCREMENT,
      user_id INT NOT NULL,
      store_id INT NOT NULL,
      click_flg BOOLEAN NOT NULL DEFAULT FALSE,
-     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP 
+                ON UPDATE CURRENT_TIMESTAMP,
      PRIMARY KEY(notice_id),
      CONSTRAINT fk_noticeLg_users
      FOREIGN KEY (user_id) 
@@ -93,12 +97,16 @@ CREATE TABLE notice_logs(
      REFERENCES stores(store_id)
 );
 
--- 決算ログ
+
+-- 決算ログ　主キーを追加
 CREATE TABLE payment_logs(
+	payment_id INT AUTO_INCREMENT,
      user_id INT NOT NULL,
      store_id INT NOT NULL,
-     used_point INT,
-     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     user_point INT,
+     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP 
+                ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY(payment_id),
      CONSTRAINT fk_paymentLg_users
      FOREIGN KEY (user_id) 
      REFERENCES users(user_id),
@@ -106,6 +114,5 @@ CREATE TABLE payment_logs(
      FOREIGN KEY (store_id) 
      REFERENCES stores(store_id)
 );
-
 
 
--- project/backend/fastapi_mysql/docker/mysql/initdb.d/data.sql
+++ project/backend/fastapi_mysql/docker/mysql/initdb.d/data.sql
@@ -1,2 +1,15 @@
 INSERT INTO users (fcm_token,mail,password) VALUES ("xxxxxxxxxx", "hogehoge@gmail.com","xxxxxxxxxx");
 
+INSERT INTO stores (name,start_at,end_at,address,content,qr_path,latitude,longitude,opening_at) VALUES ('test', '00:00:00', '00:00:00', 'hogehoge@gmail.com', 'コメント', 'http://qr.co.jp', '34.741612', '135.637207', '2019-04-17');
+
+INSERT INTO products (store_id, name,img_path) VALUES ('3', 'xxxxxxxxxx', 'http://img.co.jp');
+
+INSERT INTO events (name, start_at, end_at, func_name, point) VALUES ('xxxxxxxxxx', '2014-08-06 21:15:49', '2014-08-06 21:15:49', 'xxxxxxxxxx', '0');
+
+INSERT INTO  event_logs (user_id, event_id) VALUES("1", "1");
+
+INSERT INTO stamps (user_id, img_path) VALUES("1", "http://image.co.jp");
+
+INSERT INTO notice_logs (user_id, store_id, click_flg) VALUES("1", "3", False);
+
+INSERT INTO payment_logs (user_id, store_id, user_point) VALUES("1", "3", "100");
